078b51d5c149dfda6edc3cbe96fbdded0cc10e10
KJakubowicz <kjakubowicz98@interia.pl>
2023-02-14 23:01:29 +0100

    Create list for payments

--- app/src/Controller/PaymentsController.php
+++ app/src/Controller/PaymentsController.php
@@ -4,15 +4,60 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Builder\ListView\Builder\Paymants\PaymansListBuilder;
+use App\Builder\ListView\Maker\ListMaker;
 
 class PaymentsController extends AbstractController
 {
-    #[Route('/payments', name: 'app_payments')]
-    public function index(): Response
+    public function list(): Response
     {
-        return $this->render('payments/index.html.twig', [
+        $listBuilder = new PaymansListBuilder();
+        $listBuilder->addButton([
+            'type' => 'add',
+            'properties' => [
+                'href' => '/payment/add',
+                'name' => 'Dodaj',
+                'icon' => 'fa-solid fa-circle-plus'
+            ],
+        ]);
+        $listBuilder->addHeaderElement(
+            [
+                'class' => 'small',
+                'text' => 'LP.'
+            ],
+        );
+        $listBuilder->addHeaderElement(
+            [
+                'class' => 'basic',
+                'text' => 'Imię'
+            ],
+        );
+        $listBuilder->addHeaderElement(
+            [
+                'class' => 'basic',
+                'text' => 'Nazwisko'
+            ],
+        );
+        $listBuilder->addHeaderElement(
+            [
+                'class' => 'small',
+                'text' => 'Opcje'
+            ],
+        );
+        $rows = [];
+        $listBuilder->setRows($rows);
+        $listBuilder->setPaggination(0);
+        $listView = new ListMaker($listBuilder);
+
+        return $this->render('listView/listView.html.twig', [
             'controller_name' => 'PaymentsController',
+            'listView' => $listView->makeList(),
+            'breadcrumbs' => [
+                [
+                    'name' => 'Znajomi',
+                    'href' => '#'
+                ]
+            ],
         ]);
     }
 }
