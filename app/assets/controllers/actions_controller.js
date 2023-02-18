23f0ac1c863fcd2d84829cd598404274d0c3dfba
KJakubowicz <kjakubowicz98@interia.pl>
2023-02-18 15:13:51 +0100

    Create select layout

---
+++ app/assets/controllers/actions_controller.js
@@ -0,0 +1,37 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+    showHideSelect(event = "", element = "") {
+        let optionsBox = "";
+        if (element === "") {
+            const target = event.target;
+            optionsBox = document.getElementById("select-target");
+        } else {
+            optionsBox = element;
+        }
+
+        const visable = getComputedStyle(optionsBox).display;
+        const display = visable === "none" ? "flex" : "none";
+        optionsBox.style.display = display;
+    }
+
+    setSelectValue(event) {
+        const value = event.target.getAttribute("data-value");
+        console.log(event);
+        const activeElement = event.target.parentElement.previousSibling;
+        activeElement.replaceChildren(value);
+        const select = activeElement.parentElement.previousSibling;
+        const attribute = select.getAttribute("name");
+        const test = activeElement.nextSibling;
+        if (attribute) {
+            const attributeIndex = attribute.substring(
+                attribute.indexOf(),
+                attribute.lastIndexOf("[")
+            );
+            const newAttribute = attributeIndex + "[" + value + "]";
+            select.setAttribute("name", newAttribute);
+        }
+
+        this.showHideSelect("", activeElement.nextSibling);
+    }
+}
--- app/assets/controllers/select_controller.js
+++ app/assets/controllers/select_controller.js
@@ -6,7 +6,8 @@ export default class extends Controller {
         const options = document.getElementsByTagName("option");
         const selects = document.getElementsByTagName("select");
         this.hideSelects(selects);
-        this.prepareStructure(options);
+        const div = this.prepareStructure(options);
+        this.element.appendChild(div);
     }
 
     hideSelects(selects) {
@@ -18,18 +19,37 @@ export default class extends Controller {
     prepareStructure(options) {
         const div = document.createElement("div");
         div.setAttribute("class", "select-content");
+        div.setAttribute("data-controller", "actions");
+        div.setAttribute("data-action", "click->actions#showHideSelect");
         let span = document.createElement("span");
+
+        const divOptionsBox = document.createElement("div");
+        divOptionsBox.setAttribute("class", "select-options-box");
+        divOptionsBox.setAttribute("id", "select-target");
+
+        const divActive = document.createElement("div");
+        divActive.setAttribute("class", "select-active");
+        div.append(divActive);
+
         for (let index = 0; index < options.length; index++) {
+            let position = 30 * (index + 1);
             const element = options[index];
             let divChild = document.createElement("div");
             let span = document.createElement("span");
-            let value = element.value;
-            divChild.setAttribute("class", "select-child");
+            let value = "nowy" + index;
+            divChild.setAttribute("class", "select-child hover-select");
+            divChild.setAttribute("data-controller", "actions");
+            divChild.setAttribute(
+                "data-action",
+                "click->actions#setSelectValue"
+            );
+            divChild.setAttribute("data-value", value);
+            divChild.setAttribute("style", "top:" + position + "px");
             span.append(value);
             divChild.append(span);
-            div.append(divChild);
-            this.element.appendChild(div);
+            divOptionsBox.append(divChild);
         }
-        this.element.appendChild(div);
+        div.append(divOptionsBox);
+        return div;
     }
 }
