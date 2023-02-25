2a3bc7d82652a4e58cf52a803ad4c1844fc75f2d
KJakubowicz <kjakubowicz98@interia.pl>
2023-02-25 14:27:09 +0100

    fix menu

--- app/assets/controllers/menu_controller.js
+++ app/assets/controllers/menu_controller.js
@@ -2,11 +2,19 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
     showHideList(event) {
-        const display =
-            event.target.previousSibling.previousElementSibling.style.display;
+        const menuElement = event.target.previousSibling.previousElementSibling;
+        const display = menuElement.style.display;
+        const arrow =
+            menuElement.previousElementSibling.lastChild.previousSibling;
+        let style = "flex";
+        let arrowClass = "fa-solid fa-caret-up";
 
-        const style = display === "flex" ? "none" : "flex";
-        event.target.previousSibling.previousElementSibling.style.display =
-            style;
+        if (display === "flex") {
+            style = "none";
+            arrowClass = "fa-solid fa-caret-down";
+        }
+
+        menuElement.style.display = style;
+        arrow.setAttribute("class", arrowClass);
     }
 }
