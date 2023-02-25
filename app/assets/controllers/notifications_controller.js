add16dee1006e667eb1bca2a9dfe2975a6cb8b5a
KJakubowicz <kjakubowicz98@interia.pl>
2023-02-25 21:41:33 +0100

    Added new services for response and create new doc

--- app/assets/controllers/notifications_controller.js
+++ app/assets/controllers/notifications_controller.js
@@ -2,9 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
     async connect() {
-        const res = await fetch(
-            "http://beta.receipt.pl/api/v1/notifications/get"
-        );
+        const res = await fetch("http://beta.receipt.pl/api/notifications/get");
         let div = document.createElement("div");
         div.setAttribute("class", "notification-count");
         const data = await res.json();
